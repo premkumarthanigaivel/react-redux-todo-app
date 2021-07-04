7b43689cfebc044234d37ab9dc5b3b737cc2c3ac
Prem Kumar Thanigaivel <tprem990@gmail.com>
2021-07-04 23:46:23 +0530

    Updates

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import todoReducer from '../components/TodoSlice'
-
-export const store = configureStore({
-  reducer: {
-    todo: todoReducer
-  }
-})
